cf98220a1ee82f106ece26515e9e0b379f36ff77
Marcus Jessnitz <alliance2000@Marcuss-iMac.local>
2018-01-08 20:25:45 -0700

    Sound View Controller set up #2 ...

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by Marcus Jessnitz on 08.01.18.
+//  Copyright © 2018 Marcus Jessnitz. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+}
